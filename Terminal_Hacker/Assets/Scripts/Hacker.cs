5a9a2735caee794b4fa761dbd718fbb7bc025488
sunilpargi <49692267+sunilpargi@users.noreply.github.com>
2020-02-06 23:45:21 +0530

    Implemented Enum

--- Terminal_Hacker/Assets/Scripts/Hacker.cs
+++ Terminal_Hacker/Assets/Scripts/Hacker.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int level;
+    enum Screen { Mainmenu, Password, Win };
+    Screen currentScreen = Screen.Mainmenu;
+   
     void Start()
     {
-        Terminal.WriteLine("");
+        ShowMainMenu();
     }
 
     void ShowMainMenu()
     {
+        Terminal.ClearScreen();
         Terminal.WriteLine("Hello");
+        Terminal.WriteLine("What Would you like to hack into");
+        Terminal.WriteLine("Press 1 for the local library");
+        Terminal.WriteLine("Press 2 for the Police");
+        Terminal.WriteLine("Press 3 for the Nasa");
     }
     
     void OnUserInput(string input)
@@ -24,11 +32,31 @@ public class Hacker : MonoBehaviour
 
         else if(input == "007")
         {
-            Terminal.WriteLine("mr.bond enter correct option");
+            Terminal.WriteLine("mr.bond Please Enter The correct option");
         }
+
+        else if (input == "1")
+        {
+            level = 1;
+            Startgame();
+        }
+
+        else if(input == "2")
+        {
+            level = 2;
+            Startgame();
+        }
+
+
         else
         {
             Terminal.WriteLine("Please choose valid option");
         }
+    }
+
+    void Startgame()
+    {
+        currentScreen = Screen.Password;
+        Terminal.WriteLine("You Have Chosen:" + level);
     }
 }
